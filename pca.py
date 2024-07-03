c79f48e69311dfcf390c68b967bea7a7c8fe1302
Luisa <103768169+luisa-li@users.noreply.github.com>
2024-07-03 23:10:06 +0200

    fix

--- pca.py
+++ pca.py
@@ -1,7 +1,21 @@
 import numpy as np 
-from sklearn.datasets import load_breast_cancer
 
-def pca(D: np.ndarray, components: int):
+def pca(D: np.ndarray, components: int) -> np.ndarray:
+    """
+    Module for principal componenet analysis.
+
+    Parameters
+    ----------
+    D : np.ndarray
+        The array to perform PCA on
+    components : int
+        The number of principal components use.
+
+    Returns
+    -------
+    np.ndarray
+        The array D projected onto the principal components.
+    """
     
     means = np.mean(D, axis=0)
     std = (sum((i - means)**2 for i in D)/len(D))**0.5
